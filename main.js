3c0544ce39d939668c57147059b752e0a86ecf6e
NT1210 <nao_annms@yahoo.co.jp>
2023-12-14 14:59:42 +0900

    splitted codes to utils/puppeteer.js

--- main.js
+++ main.js
@@ -1,77 +1,12 @@
 
-const puppeteer = require("puppeteer")
-
-function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
- }
+const {extract} = require("./utils/puppeteer")
 
 async function main(){
-    const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe"
-    })
-    
-    const page = await browser.newPage()
+    const extractedData = await extract()
 
-    // By making use of page.exporseFunction, you can use this function inside page.evaluate()
-    await page.exposeFunction("sliceByNumber", (array, number) => {
-        const length = Math.ceil(array.length / number)
+    // below output extractedData to template excel file. and hopefully get it connected to DB.
 
-        return new Array(length).fill().map((_, i) =>
-          array.slice(i * number, (i + 1) * number)
-        )
-    })
-    
-    await page.goto("https://ciferquery.singlewindow.cn/")
-
-    await page.type('#registerTypeName', "18")
-
-    await delay(1500)
-
-    await page.type('#countryName', "俄罗斯-Russia")
-
-    await delay(1500)
-
-    await page.click('span[id="chaxun"]')
-
-
-    const lastPageNum = await page.evaluate(() => {
-        let links = document.querySelectorAll(".page-item")
-        // Be careful so as not to do .slice(-1). The last element is ">"
-        return Array.from(links).slice(-2)[0].textContent
-    })
-
-
-    let extractedArr = []
-
-    for(let i=0; i<lastPageNum; i++){
-        const dataPerPage = await page.evaluate(() => {
-
-            let tableBody = document.querySelectorAll("tbody td")
-            let temparr = []
-            tableBody.forEach(ele => temparr.push(ele.textContent))
-            temparr.shift()
-            let slicedArr = window.sliceByNumber(temparr, 10)
-
-            return slicedArr
-        })
-
-        extractedArr.push(dataPerPage)
-    
-        delay(1500)
-        await page.click(".page-next a")
-    }
-
-    // The arr structure of extractedArr is [[[ele1], [ele2]...], [[ele1], [ele2]...]...]
-    // By flatteing, you can create arr such as [[ele1], [ele2], [ele3]...]
-    flattedArr = extractedArr.flat()
-    console.log(flattedArr)
-
-    browser.close()
-
-    return flattedArr
+    // return extractedData
 }
 
 
